4d2a90e5aa1c2f9db863dc41967c0d91d73e66e4
Rick van Rein <rick@openfortress.nl>
2016-09-23 15:36:08 +0200

    Removed own/empty templates

--- source/conf.py
+++ source/conf.py
@@ -54,7 +54,7 @@ extensions = [
 # -- RvR.END
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+#NOTYET-READTHEDOCS# templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
